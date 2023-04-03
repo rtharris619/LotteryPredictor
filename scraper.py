ad758a8156084f95778177b98d24b7754e8b93c4
Ryan Harris <ryan@synthesis.co.za>
2023-04-03 10:27:10 +0200

    Lottery Predictor successfully scraping data.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import datetime
 
 # For a list of winners from 5 years ago:
 # https://www.lottoland.co.za/lotto/results-winning-numbers
@@ -9,38 +11,54 @@ import requests
 
 
 def driver():
-    lottery_scraper = LotteryScraper(2023)
-    lottery_scraper.get_results()
+    lottery_scraper = LotteryScraper()
+    # lottery_scraper.scrape()
 
 
 class LotteryItem:
-    def __init__(self, date, draw_number, balls, bonus_ball, jackpot):
+    def __init__(self, date, draw_number, ball1, ball2, ball3, ball4, ball5, ball6, bonus_ball, jackpot):
         self.date = date
         self.draw_number = draw_number
-        self.balls = balls
+        self.ball1 = ball1
+        self.ball2 = ball2
+        self.ball3 = ball3
+        self.ball4 = ball4
+        self.ball5 = ball5
+        self.ball6 = ball6
         self.bonus_ball = bonus_ball
         self.jackpot = jackpot
 
-    def __repr__(self):
-        result = 'Balls: '
-        for ball in self.balls:
-            result += ball + '; '
-        return f'{result}'
-
 
 class LotteryScraper:
-    def __init__(self, year):
+    def __init__(self):
         self.soup = None
-        self.base_url = f'https://www.lottery.co.za/lotto/results/{year}'
-        self.scrape_webpage()
+        self.base_url = f'https://www.lottery.co.za/lotto/results'
+        self.current_year = 2023
+        self.lottery_data = []
 
-    def scrape_webpage(self):
-        html = self.get_html()
+    def scrape(self):
+        """Scrape function to scrape all the pages."""
+        self.scrape_pages()
+        self.write_to_csv(self.lottery_data)
+
+    def scrape_pages(self):
+        self.scrape_webpage(f'{self.base_url}/{self.current_year}')
+        anchors = self.soup.find_all('a', attrs={'class': 'button-blue'})
+
+        for a in anchors:
+            year = a.get_text()
+            print(f'starting processing for {year}')
+            self.scrape_webpage(f'{self.base_url}/{year}')
+            self.get_results()
+            print(f'finished processing for {year}')
+
+    def scrape_webpage(self, url):
+        html = self.get_html(url)
         if html:
             self.soup = BeautifulSoup(html, 'html.parser')
 
-    def get_html(self):
-        r = requests.get(self.base_url)
+    def get_html(self, url):
+        r = requests.get(url)
         if r:
             results = r.text
             if results:
@@ -53,8 +71,6 @@ class LotteryScraper:
         body = result.find('tbody')
         trs = body.find_all('tr')
 
-        data = []
-
         for tr in trs:
             date = None
             draw_number = None
@@ -64,7 +80,8 @@ class LotteryScraper:
             tds = tr.find_all('td')
             for i, td in enumerate(tds):
                 if i == 0:
-                    date = td.getText(strip=True)
+                    date_string = td.getText(strip=True)
+                    date = self.convert_to_datetime(date_string)
                 elif i == 1:
                     draw_number = td.getText(strip=True)
                 elif i == 2:
@@ -75,9 +92,26 @@ class LotteryScraper:
                     bonus = td.find('div', class_='lotto-bonus-ball')
                     bonus_ball = bonus.get_text()
                 elif i == 3:
-                    jackpot = td.getText(strip=True)
+                    jackpot_str = td.getText(strip=True)
+                    jackpot = self.convert_to_float(jackpot_str)
 
-            lottery_item = LotteryItem(date, draw_number, ball_numbers, bonus_ball, jackpot)
-            data.append(lottery_item)
+            lottery_item = LotteryItem(date, draw_number, ball_numbers[0], ball_numbers[1], ball_numbers[2],
+                                       ball_numbers[3], ball_numbers[4], ball_numbers[5],
+                                       bonus_ball, jackpot)
+            self.lottery_data.append(lottery_item)
 
-        print(data[0])
+        # self.write_to_csv(data)
+
+    def convert_to_datetime(self, date_str):
+        format = '%A, %d %B %Y'
+        datetime_str = datetime.datetime.strptime(date_str, format)
+
+        return datetime_str
+
+    def convert_to_float(self, flt_str):
+        flt_str = ''.join(c for c in flt_str if c.isdigit())
+        return float(flt_str)
+
+    def write_to_csv(self, data):
+        df = pd.DataFrame([vars(x) for x in data])
+        df.to_csv('./LotteryResults.csv', index=False)
